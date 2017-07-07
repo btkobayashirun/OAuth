162c27bb31e4862509e4163a3fc252467425c1a5
btkobayashirun <kobayashirun@pm.nttdata.co.jp>
2017-07-07 11:40:59 +0900

    investigation of simultaneous use for spel of normal and oauth.

--- oauth/oauth-env/src/main/resources/database/H2-dataload.sql
+++ oauth/oauth-env/src/main/resources/database/H2-dataload.sql
@@ -1,7 +1,7 @@
 /* load the records. */
 /* Resource Owner Records. password=demo */
 INSERT INTO users(username, password, enabled) VALUES('demo', '$2a$10$oxSJl.keBwxmsMLkcT9lPeAIxfNTPNQxpeywMrF7A3kVszwUTqfTK', true);
-INSERT INTO authorities(username, authority) VALUES('demo', 'READ');
+INSERT INTO authorities(username, authority) VALUES('demo', 'ROLE_READ');
 
 /* Client Records. */
 INSERT INTO clients(client_id, client_secret, client_name, access_token_validity, refresh_token_validity) VALUES('testClient', '$2a$10$oxSJl.keBwxmsMLkcT9lPeAIxfNTPNQxpeywMrF7A3kVszwUTqfTK', 'hogehoge', 10, 20);
